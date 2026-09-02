de951c0a41fbec8f62cac0e8a6d1c48cc8d28add
Samuel Araújo <samusilvadev@gmail.com>
2026-09-02 17:46:01 -0300

    migration: expose legacy target classifier

---
+++ src/research_os/legacy/__init__.py
@@ -0,0 +1,3 @@
+from research_os.legacy.formolecular import LegacyTargetClass, classify_legacy_target, migration_plan_for_targets
+
+__all__ = ["LegacyTargetClass", "classify_legacy_target", "migration_plan_for_targets"]
