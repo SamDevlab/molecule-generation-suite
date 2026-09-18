9b88835865a6d6d2c85efe44336fa28a385324cf
Samuel Araújo <samusilvadev@gmail.com>
2026-09-17 23:01:37 -0300

    feat(moldisc-006): expose selected docking program API

--- src/research_os/molecular_discovery/__init__.py
+++ src/research_os/molecular_discovery/__init__.py
@@ -49,6 +49,11 @@ from research_os.molecular_discovery.moldisc005 import (
     generate_nct_n_alkyl_series,
     run_moldisc_005,
 )
+from research_os.molecular_discovery.moldisc006 import (
+    MOLDISC006Error,
+    MOLDISC006Result,
+    run_moldisc_006,
+)
 from research_os.molecular_discovery.solubility import (
     FrozenESOLSolubilityPredictor,
     SolubilityCapabilityError,
@@ -71,6 +76,8 @@ __all__ = [
     "GeneratedNCTAnalog",
     "GenerationReport",
     "MOLDISC001Result",
+    "MOLDISC006Error",
+    "MOLDISC006Result",
     "MOLDISC002Error",
     "MOLDISC002Result",
     "MOLDISC003Error",
@@ -93,6 +100,7 @@ __all__ = [
     "generate_halogen_analogs",
     "generate_nct_n_alkyl_series",
     "run_moldisc_001",
+    "run_moldisc_006",
     "run_moldisc_002",
     "run_moldisc_003",
     "run_moldisc_004",
